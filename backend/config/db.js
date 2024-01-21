76c376fcb88bdede0e7078041940490574869692
hugo-ravailhe <hugo.rh@free.fr>
2024-01-21 17:53:43 +0100

    create kubernetes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'database_heale', 
+  host: 'database-heale', 
   port: '3306',
   user: 'root',
   password: process.env.MYSQL_ROOT_PASSWORD,
